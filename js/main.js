8bdaa1e9078d38331ae53382d409daf36ee49c3e
moonrailgun <moonrailgun@gmail.com>
2017-01-18 14:33:43 +0800

    搜索框

--- js/main.js
+++ js/main.js
@@ -1,29 +1,35 @@
 $(function() {
   Search.toggleVisibility();
   $('.search').bind('keyup change',function(event) {
-    var searchStr = Search.getCurrentVal();
+    var keycode = event.keyCode;
+    if(keycode == 38 ||keycode == 40){
+      return;
+    }
 
-    $.ajax({
-      url: 'http://suggestion.baidu.com/',
-      type: 'GET',
-      dataType: 'text',
-      data: {
-        wd: searchStr,
-        json:1,
-        cb:"callback"
-      },
-      success:function(data){
-        var text = data.match(/callback\((.*)\);/)[1];
-        var json;
-        try {
-          json = window.eval("(" + text + ")");
-        } catch (s) {
-          json = JSON.parse(text);
+    var searchStr = Search.getCurrentVal();
+    if(searchStr != ""){
+      $.ajax({
+        url: 'http://suggestion.baidu.com/',
+        type: 'GET',
+        dataType: 'text',
+        data: {
+          wd: searchStr,
+          json:1,
+          cb:"callback"
+        },
+        success:function(data){
+          var text = data.match(/callback\((.*)\);/)[1];
+          var json;
+          try {
+            json = window.eval("(" + text + ")");
+          } catch (s) {
+            json = JSON.parse(text);
+          }
+          var sugArr = json.s;
+          Search.updateSuggestions(sugArr);
         }
-        var sugArr = json.s;
-        Search.updateSuggestions(sugArr);
-      }
-    })
+      });
+    }
   });
 
   $('.search').bind('search',function(e){
@@ -32,8 +38,45 @@ $(function() {
       console.log('搜索内容为空');
     }else{
       // console.log(searchText);
-      var url = "https://www.baidu.com/s?wd=" + encodeURI(searchText);
-      window.open(url, "_self");
+      Search.openSearchWindow(searchText);
+    }
+  });
+
+  $('.search').keydown(function(event) {
+    //为兼容中文输入法不能与keyup事件公用
+    if(event.keyCode == 38){
+      //up
+      var oldActiveItem = $('.suggestions .suggestion.active');
+      var newActiveItem = oldActiveItem.prev('.suggestion');
+      if(newActiveItem[0]){
+        oldActiveItem.removeClass('active');
+        newActiveItem.addClass('active');
+      }else{
+        oldActiveItem.removeClass('active');
+        oldActiveItem.nextAll().last().addClass('active');
+      }
+      Search.setCurrentVal(newActiveItem.text());
+    }else if(event.keyCode == 40){
+      //down
+      var oldActiveItem = $('.suggestions .suggestion.active');
+      var newActiveItem = oldActiveItem.next();
+      if(newActiveItem[0]){
+        oldActiveItem.removeClass('active');
+        newActiveItem.addClass('active');
+      }else{
+        oldActiveItem.removeClass('active');
+        oldActiveItem.prevAll().last().addClass('active');
+      }
+      Search.setCurrentVal(newActiveItem.text());
+    }
+  });
+
+  $('.search').on('mousedown','.suggestion', function(event) {
+    var target = $(event.target);
+    console.log(target);
+    if(target.hasClass('suggestion')){
+      var searchText = target.text();
+      Search.openSearchWindow(searchText);
     }
   });
 })
--- js/search.js
+++ js/search.js
@@ -3,18 +3,25 @@ $(function(){
     getCurrentVal:function(){
       return $('.search input').val();
     },
+    setCurrentVal:function(str){
+      $('.search input').val(str)
+    },
     toggleVisibility:function(){
       $('.suggestions').toggleClass('visibility');
     },
     updateSuggestions:function(arr){
+      var currentVal = this.getCurrentVal()
       $('.suggestions').html('');
-      $('.suggestions').append('<div class="suggestion active">'+ this.getCurrentVal() +'</div>');
-      // foreach(var item in arr){
-      //   $('.suggestions').append('<div class="suggestion">'+item+'</div>');
-      // }
-      $.each(arr,function(index, el) {
-        $('.suggestions').append('<div class="suggestion">'+el+'</div>');
+      $('.suggestions').append('<div class="suggestion active">'+ currentVal +'</div>');
+      $.each(arr, function(index, el) {
+        if(currentVal != el){
+          $('.suggestions').append('<div class="suggestion">'+el+'</div>');
+        }
       });
+    },
+    openSearchWindow:function(searchText){
+      var url = "https://www.baidu.com/s?wd=" + encodeURI(searchText);
+      window.open(url, "_self");
     }
   }
 
