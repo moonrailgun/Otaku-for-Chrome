94b6e1639c1a4ee506b99fd43e69f4739d019abb
moonrailgun <moonrailgun@gmail.com>
2017-01-23 16:41:19 +0800

    仅可通过单击打开一个标签页

--- js/background.js
+++ js/background.js
@@ -1,9 +1,27 @@
 (function(){
 	var appURL = chrome.extension.getURL("index.html");
+	var appTabId = 0;
 
-	chrome.browserAction.onClicked.addListener(function() {
+	var createAppTab = function(){
 		chrome.tabs.create({
 			url: appURL
+		},function(tab){
+			appTabId = tab.id;
 		});
+	}
+
+	chrome.browserAction.onClicked.addListener(function() {
+		if(appTabId != 0){
+			chrome.tabs.get(appTabId,function(tab){
+				if(tab){
+					chrome.tabs.highlight({tabs:tab.index});
+				}
+				else{
+					createAppTab();
+				}
+			});
+		}else{
+			createAppTab();
+		}
 	});
 })();
