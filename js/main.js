da96f9def0d5c3202b0f07fe3173da26c29d75a8
moonrailgun <moonrailgun@gmail.com>
2017-10-17 08:54:41 +0800

    增加颜色命名模块

--- js/main.js
+++ js/main.js
@@ -203,59 +203,6 @@ $(function() {
     Core.setWallPaper(lastWallPaperUrl);
   }
 
-  //初始化widgetLayout
-  var defaultWidgetLayout = [
-    {
-      name:"weather",
-      label:"天气",
-      layout:"left"
-    },
-    {
-      name:"calculator",
-      label:"简易计算器",
-      layout:"left"
-    },
-    {
-      name:"tasks",
-      label:"TODO",
-      layout:"left"
-    },
-    {
-      name:"hitokoto",
-      label:"一言",
-      layout:"center"
-    },
-    {
-      name:"clock",
-      label:"时钟",
-      layout:"center"
-    },
-    {
-      name:"githubContributions",
-      label:"Github贡献",
-      layout:"close"
-    },
-    {
-      name:"bilibiliquick",
-      label:"bilibili快速入口",
-      layout:"close"
-    },
-    {
-      name:"translate",
-      label:"百度翻译",
-      layout:"right"
-    },
-    {
-      name:"bookmarks",
-      label:"书签",
-      layout:"right"
-    },
-    {
-      name:"topsites",
-      label:"最近网站",
-      layout:"close"
-    }
-  ];
   var widgetLayout = Core.settings.get("widgetLayout");
   if($.isEmptyObject(widgetLayout)){
     //如果为空则设为默认布局
